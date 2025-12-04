1fb0232457ac79008a03b2c4b1bb52e45989e8f0
GitLab CI <ci@gitlab.com>
2025-12-04 13:32:06 +0700

    Update troboz to v0.1.49

--- Formula/troboz.rb
+++ Formula/troboz.rb
@@ -6,10 +6,10 @@ class Troboz < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/KreasiMaju/troboz-release/releases/download/v0.1.49/troboz-0.1.49-macos-arm64.tar.gz"
-    sha256 ""
+    sha256 "20d93bae51e5b2d023d63e32ab8f5339706491bd376a0046b3ed6f8150fcdcd3"
   else
     url "https://github.com/KreasiMaju/troboz-release/releases/download/v0.1.49/troboz-0.1.49-macos-x86_64.tar.gz"
-    sha256 ""
+    sha256 "6abeeaa3faa003848aaa95ed9bb816d63998dd3b425a797d9e97d7d8e6b58e4c"
   end
 
   def install
